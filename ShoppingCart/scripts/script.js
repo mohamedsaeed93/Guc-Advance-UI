afdfc137562e6efbfbb6f54fb55440b0983fff7e
mohamedsaeed93 <mohamed.elsaeed93@gmail.com>
2016-04-09 01:44:56 +0200

    Improving the css and javascript based on Alfy's Feedback

--- ShoppingCart/scripts/script.js
+++ ShoppingCart/scripts/script.js
@@ -1,12 +1,7 @@
 $( document ).ready(function() {
-	$("select#payment_method").css("background-image",'url(images/'+$("select#payment_method").val()+'.png)');
-	$("select#payment_method").change(function(){
-		$("select#payment_method").css("background-image",'url(images/'+$("select#payment_method").val()+'.png)');
-	});
-
-
-	$("select#lang_choice").css("background-image",'url(images/'+$("select#lang_choice").val()+'.png)');
-	$("select#lang_choice").change(function(){
-		$("select#lang_choice").css("background-image",'url(images/'+$("select#lang_choice").val()+'.png)');
-	});
+  var $paymentMethod = $("select#payment_method");
+  $paymentMethod.css("background-image",'url(images/'+$paymentMethod.val()+'.png)');
+  $paymentMethod.change(function(){
+    $(this).css("background-image",'url(images/'+$(this).val()+'.png)');
+  });
 });
